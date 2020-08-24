f615501df23270fc06cd235e52a48b98ed1b540f
Joanjo <mtz230396@gmail.com>
2020-08-24 16:47:27 -0500

    mater

---
+++ app.py
@@ -0,0 +1,24 @@
+import os, io
+from google.cloud import vision
+from google.cloud.vision import types
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'TokenApiVision.json'
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# Path img
+file_name = os.path.abspath('img/ine.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+# construct an iamge instance
+image = vision.types.Image(content=content)
+
+
+response = client.text_detection(image=image)  # returns TextAnnotation
+
+# text_annotations
+print(response.text_annotations)
